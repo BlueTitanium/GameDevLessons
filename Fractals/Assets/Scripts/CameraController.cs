ead0be9a3d9c5025b515836dfb325a76e0d80e53
BlueTitanium <xyzftw@gmail.com>
2019-09-12 11:09:36 -0400

    Created my own simple camera movement

---
+++ Fractals/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class CameraController : MonoBehaviour {
+    public float speed = 2.0f;
+    public Transform target;
+    void Start () {}
+    void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveLengthWise = Input.GetAxis("Vertical");
+        float moveVertical = 0;
+        if(Input.GetKey("space")) moveVertical = 1;
+        if(Input.GetKey("left shift")) moveVertical = -1;
+        transform.Translate(new Vector3(moveHorizontal, moveVertical, moveLengthWise) * speed);
+        transform.LookAt(target.position);
+    }
+}
