9a41b374d4dfe7ee825a8a328618ab86731ddcae
TANEIM MIAH <MIAHT1>
2019-09-17 11:05:45 -0400

    Lesson 6: Rigidbodies
    Just the basics of rigidbodies

---
+++ Unit 1/Assets/Scripts/RollingSphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingSphere : MonoBehaviour {
+    public float speed;
+    public float flyspeed = .05f;
+    private bool flying = false;
+    private Rigidbody rb3d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb3d = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        if(Input.GetKey("tab"))flying = !flying;
+        float yMove = 0f;
+        if(Input.GetKey("space"))yMove = 1;
+        if(Input.GetKey("left shift"))yMove = -1;
+        if(flying) rb3d.AddForce(-Physics.gravity, ForceMode.Acceleration);
+        float zMove = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(xMove, 0, zMove);
+        rb3d.AddForce(movement * speed);
+        transform.Translate(0f,yMove*flyspeed,0f);
+    }
+}
