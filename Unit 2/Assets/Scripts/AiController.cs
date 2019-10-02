0f12b62f90b7fb093e078253800299c8ef5daa0a
TANEIM MIAH <MIAHT1>
2019-10-02 11:01:50 -0400

    Added a solo lesson
    I did this to learn pathfinding on my own. Self-taught!

---
+++ Unit 2/Assets/Scripts/AiController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+ 
+public class AiController : MonoBehaviour
+{
+    [SerializeField]
+    Transform destination;
+ 
+    NavMeshAgent navMeshagent;
+   
+    // Use this for initialization
+    public void Start ()
+    {
+        navMeshagent = this.GetComponent<NavMeshAgent>();
+        
+    }
+    public void Update()
+    {
+        SetDestination();
+    }
+ 
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshagent.SetDestination(targetVector);
+        }
+    }
+}
---
+++ Unit 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVal = Input.GetAxis("Horizontal");
+        float zVal = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(xVal, 0, zVal) * speed);
+    }
+}
