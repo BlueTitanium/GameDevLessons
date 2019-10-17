a62bf711eaea266d17d10472477bf80152e436a6
BlueTitanium <miaht1@bxscience.edu>
2019-10-17 11:11:00 -0400

    Added to garden scene

---
+++ Chapter 7 Assets/Garden Defender/Assets/Game Scripts/PotatoLauncher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotatoLauncher : MonoBehaviour {
+	
+	public GameObject projectile; // the projectile prefab
+	public float speed = 20f; // give speed a default of 20 
+	float loadRate = 0.5f; // how often a new projectile can be fired
+	float timeRemaining; // how much time before the next shot can happen
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		timeRemaining -= Time.deltaTime; //
+		// if the Fire1 button (default is left ctrl) is pressed and the alloted time has passed
+		if (Input.GetButton ("Fire1") && timeRemaining <= 0) { 
+		   timeRemaining = loadRate; // reset the time remaining
+		   ShootProjectile ();// …shoot the projectile
+			GetComponent<AudioSource>().Play (); // play the default audio clip on this component's gameObject
+		}
+	
+	}
+	
+	void ShootProjectile () {  
+	   // create a clone of the projectile at the location & orientation of the script's parent     
+	   GameObject potato = (GameObject) Instantiate (projectile, transform.position, transform.rotation);
+	   // add some force to send the projectile off in its forward direction
+	   potato.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3 (0,0,speed));
+	}
+
+}
--- Chapter 7 Assets/Garden Defender/Assets/Game Scripts/SimpleCharcterController.cs
+++ Chapter 7 Assets/Garden Defender/Assets/Game Scripts/SimpleCharcterController.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class SimpleCharcterController : MonoBehaviour {
 	
 	Animator animator; // the Animator paramtercomponent/ state engine
-	
+	public float rotSpeed = 2f;
+	Rigidbody rb3d;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>(); // assign the animator
+		rb3d = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (animator) {
 			if(Input.GetKey("space")){
-				animator.SetBool("Jump", true);
+				animator.SetBool("Dance", true);
+				rb3d.AddForce(0,15f,0);
 			} else {
-				animator.SetBool("Jump", false);
+				animator.SetBool("Dance", false);
 			}
 			float v = Input.GetAxis("Vertical");
+			
 		    animator.SetFloat("Input V", v);
 		    print (v); // see what the v input value sent to the animator
+		}
+		if(Input.GetKey("a")){
+			transform.Rotate(0,-1*rotSpeed,0);
+		}
+		if(Input.GetKey("d")){
+			transform.Rotate(0,rotSpeed,0);
 		}
 	}
 	
