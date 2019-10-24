8fe6614f18ad698a596f466838edcde001452e3a
BlueTitanium <miaht1@bxscience.edu>
2019-10-24 11:10:48 -0400

    edits

---
+++ Chapter 7 Assets/Garden Defender/Assets/Game Scripts/Plants.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plants : MonoBehaviour
+{
+    bool dying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dying && transform.localScale != new Vector3(0f, 0f, 0f)){
+            transform.localScale -= new Vector3(.05f, .05f, .05f);
+        }
+        if(transform.localScale == new Vector3(0f, 0f, 0f)){
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Enemies"){
+            dying = true;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Chapter 7 Assets/Garden Defender/Assets/Game Scripts/PohTahToh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PohTahToh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Background"){
+            gameObject.SetActive(false);
+        }
+    }
+}
